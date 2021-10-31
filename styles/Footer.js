0c61b9a155ec021f40105f778b5ede3357cc69c5
Suriya Ganesh <suriyaganesh097@gmail.com>
2021-10-31 12:42:07 +0530

    Fresh!!! mf!!

--- styles/Footer.js
+++
@@ -1,6 +0,0 @@
-import css from "styled-jsx/css";
-
-export const FooterStyle = css`.page-footer {
-    margin: 40px ;
-    width: 20%;
- }`
--- styles/Variables.js
+++
@@ -1,1 +0,0 @@
-export const BACKGROUND_COLOR = 'white';
